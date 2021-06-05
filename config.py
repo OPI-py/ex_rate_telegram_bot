98d2a3b99b28741ca86b428dbf330a546d6d461a
OPI-py <oleksandrp09@gmail.com>
2021-06-05 18:09:41 +0300

    Utility config file

---
+++ config.py
@@ -0,0 +1,11 @@
+# Get token from @BotFather
+token = ''
+
+# /fxmarketapi.com
+API_fxmarketapi_com = ""
+# exchangeratesapi.io
+API_exchangeratesapi_io = ""
+
+currencies = f'http://api.exchangeratesapi.io/v1/latest?access_key={API_exchangeratesapi_io}'
+
+help_start = "Hello. I am currency exchange bot:\n/list, /lst - display currencies and rates, base currency - EUR.\n/exchange - convert 10 EUR to CAD.\n/xe, /convert - convert currency.\n/history, /past - show rate histogram for chosen date."
